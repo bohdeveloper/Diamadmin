27dfe22ae15ac29617a85ff0ca71a262acbc8208
bohdeveloper <ohb.seven@gmail.com>
2022-12-23 14:38:46 +0100

    Desarrollo del login

--- src/main/resources/static/js/diamadmin.js
+++ src/main/resources/static/js/diamadmin.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
 
+    //NAVBAR
+    //Ocultar navbar
+    if($('#location').val() == "login") {
+      $('.bd-navbar').addClass('d-none');
+    }
+    console.log($('#location').val());
+
     //USUARIOS
-    
     //Doble click para editar usuarios
     $('.filaUsuarios').dblclick(function(){
       $(location).attr('href', "/app/usuarios/detalle/" + $(this).text().trim().charAt(0));
